448a916d1e3a0f52f9fbd5b4c33b28a4d0c69867
seshathri <166812881+seshathri044@users.noreply.github.com>
2025-09-02 00:07:04 +0530

    Update CourseController.java

--- src/main/java/com/example/Course/Registration/System/controller/CourseController.java
+++ src/main/java/com/example/Course/Registration/System/controller/CourseController.java
@@ -1,34 +1,32 @@
-package com.example.Course.Registration.System.controller;
-
-import com.example.Course.Registration.System.model.Course;
-import com.example.Course.Registration.System.model.CourseRegister;
-import com.example.Course.Registration.System.service.CourseService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
 @RestController
-@CrossOrigin(origins = {"http://localhost:5500", "http://127.0.0.1:5500","https://course-register-system.netlify.app"})
+@CrossOrigin(
+    origins = {
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://course-register-system.netlify.app"
+    },
+    methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}
+)
 public class CourseController {
 
     @Autowired
     CourseService courseService;
 
-    @GetMapping("courses")
-    public List<Course> availableCourses(){
+    @GetMapping("/courses")
+    public List<Course> availableCourses() {
         return courseService.availableCourses();
     }
-    @GetMapping("courses/enrolled")
-    public List<CourseRegister> enrolledStudent(){
+
+    @GetMapping("/courses/enrolled")
+    public List<CourseRegister> enrolledStudent() {
         return courseService.enrolledStudent();
     }
+
     @PostMapping("/courses/register")
     public String enrolledCourse(@RequestParam("name") String name,
                                  @RequestParam("emailId") String emailId,
-                                 @RequestParam("courseName") String courseName){
-        courseService.enrollCourse(name,emailId,courseName);
-        return "Congratulation!"+ name +"Enrollment Successful for"+courseName;
-
+                                 @RequestParam("courseName") String courseName) {
+        courseService.enrollCourse(name, emailId, courseName);
+        return "Congratulation! " + name + " Enrollment Successful for " + courseName;
     }
 }
